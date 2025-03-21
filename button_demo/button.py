2f61030c2a2351b53524bf07470c008b7ef98877
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-03-21 13:12:53 +0100

    Refined button demo

--- button_demo/button.py
+++ button_demo/button.py
@@ -1,15 +1,16 @@
 from py4godot.classes import gdclass
 from py4godot.classes.core import Vector2, Rect2
-from py4godot.classes.Button import Button
+from py4godot.classes.Button.Button import Button
 
 @gdclass
 class button(Button):
-	def _ready(self) -> None:
-		print("hello")
-	
-	
-	def _pressed(self) -> None:
-		print("Pressed")
-
 	def _has_point(self, point):
 		return Rect2.new3(Vector2.new0(), self.get_size()).has_point(point)
+
+	# This works handles clicks, when connecting the signal pressed()
+	def _on_pressed(self):
+		print("Hello world")
+	
+	# You could also do it like this, if you don't want to connect the signal pressed()
+	# def _pressed(self):
+	# 	print("Hello world")
