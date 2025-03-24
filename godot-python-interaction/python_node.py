df70d0371d18b77cb3d88648c1a97c12186b6309
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-03-24 16:18:14 +0100

    Fixed example

--- godot-python-interaction/python_node.py
+++ godot-python-interaction/python_node.py
@@ -1,6 +1,3 @@
-
-from py4godot.methods import private
-from py4godot.signals import signal, SignalArg
 from py4godot.classes import gdclass
 from py4godot.classes.core import Vector3
 from py4godot.classes.Node3D.Node3D import Node3D
